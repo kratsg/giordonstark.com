ad0b5f3a3280c5c9be97706425e3beaf1b9f7597
Giordon Stark <kratsg@gmail.com>
2026-04-11 17:00:28 -0700

    fix prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,26 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+// eslint.config.js
+import { defineConfig } from "eslint/config";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
+import eslintPluginAstro from "eslint-plugin-astro";
 
-export default [
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
+export default defineConfig([
+  {
+    ignores: ["public/**"]
+  },
+  // Use .gitignore
+  includeIgnoreFile(gitignorePath),
+
+  // Astro recommended config
   ...eslintPluginAstro.configs.recommended,
+
+  // addtional custom rules
   {
     rules: {
       // Allow unused variables prefixed with _ (common in Astro frontmatter)
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     },
   },
-];
+]);
